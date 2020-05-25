e09f0eb9ccfcd1061bb73bbc16e565f01549b01b
Yar0509 <yaroslav.glyva@gmail.com>
2020-05-25 18:16:51 +0300

    asd

---
+++ T12105.js
@@ -0,0 +1,19 @@
+/*1. Напишите функцvar array = [1,2,3];
+console.log(array[0]);
+
+function copyArr() {
+var clone = {};
+for(var arr in array){
+    if(array.hasOwnProperty(arr)){
+        clone(arr)=array(arr);
+        }
+    }
+}
+console.log(copyArr)ию copyArr(arr),
+которая копирует массив не изменяя оригинал.*/
+
+
+
+/*я пытался создать функцию,
+которая будет клонировать массив
+но чё-то получилась какая-то ерундень...*/
---
+++ T32105.js
@@ -0,0 +1,12 @@
+/*3. Дан объект {js:'test', jq: 'hello', css: 'world'}.
+Получите массив его ключей.*/
+
+var obj = {
+    js: 'test',
+    jq: 'hello',
+    css: 'world'
+}
+
+var array = [obj];
+console.log(array[0]);
+
